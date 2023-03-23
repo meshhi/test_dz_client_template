e460e32a7f41460764ce88bee0cccaafe1e9b115
m.podosenovmiac <podosenovmiac@yandex.com>
2023-03-23 11:00:14 +0300

    init

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -15,7 +15,6 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import { useState } from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
 // javascipt plugin for creating charts
@@ -48,6 +47,15 @@ import {
 
 import Header from "components/Headers/Header.js";
 
+
+
+import { useEffect, useRef, useState, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import fetchAllPersons from "../store/async-thunks/persons-thunk";
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
 const Index = (props) => {
   const [activeNav, setActiveNav] = useState(1);
   const [chartExample1Data, setChartExample1Data] = useState("data1");
@@ -61,12 +69,49 @@ const Index = (props) => {
     setActiveNav(index);
     setChartExample1Data("data" + index);
   };
+
+
+  const gridRef = useRef();
+  const [rowData, setRowData] = useState([
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxster", price: 72000}
+  ]);
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'make', filter: true},
+    {field: 'model', filter: true},
+    {field: 'price'}
+  ]);
+
+  const defaultColDef = useMemo( ()=> ({
+    sortable: true
+  }));
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllPersons());
+  }, []);
+
+
+
   return (
     <>
       {/* <Header /> */}
       {/* Page content */}
       <Container className="mt--7" fluid>
+        <AgGridReact
+          ref={gridRef} // Ref for accessing Grid's API
 
+          rowData={rowData} // Row Data for Rows
+
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+
+          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+          rowSelection='multiple' // Options - allows click selection of rows
+
+          onCellClicked={() => console.log('cell clicked')} // Optional - registering for Grid Event
+        />
       </Container>
     </>
   );
